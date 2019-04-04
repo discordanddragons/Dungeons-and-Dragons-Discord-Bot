30ee8e15729f5031b6388c74e21f5bb571ec898e
Afropenguin <mbrooks8@ucmerced.edu>
2019-04-03 23:41:58 -0700

    Language Adding fix
    prepending language_ to all dm created languages and allowing users to only add languages that were made by the dm

--- dnd.py
+++ dnd.py
@@ -7,12 +7,13 @@ TOKEN = 'NTYzMDUxMTA2ODkzMDM3NTk4.XKTzBA.kCuGuv8Onok8NZZm1Q5TfPfrGAc'
 
 client = discord.Client()
 
+
 @client.event         
 async def on_voice_state_update(before, after):
     isDM = False
     isPlayer = False
     for role in before.roles:
-        print (role.name)
+        print(role.name)
         if role.name == "Bots":
             return
         elif role.name == "DM":
@@ -26,9 +27,10 @@ async def on_voice_state_update(before, after):
         for member in client.get_all_members():
             if member == before or member.voice.voice_channel == None:
                 continue
-            await client.move_member(member,destChannel)
-            print ("moving ",member," to: ",destChannel)
-        
+            await client.move_member(member, destChannel)
+            print("moving ", member, " to: ", destChannel)
+
+
 @client.event         
 async def on_message(message):
     if message.author == client.user:
@@ -40,10 +42,10 @@ async def on_message(message):
             isDM = True
         if role.name == "Player":
             isPlayer = True
-    #administrator/DM commands
+    # administrator/DM commands
     if message.author.server_permissions.administrator or isDM:        
         if message.content.startswith("!registerLanguage"):
-            channelname = message.content.replace("!registerLanguage","")
+            channelname = "language_" + message.content.replace("!registerLanguage", "").lower()
             newChannel = await client.create_channel(message.server, channelname, type=discord.ChannelType.voice)
             role = await client.create_role(message.server, name=channelname)
             newRole = await client.add_roles(message.author, role)
@@ -51,25 +53,31 @@ async def on_message(message):
             newChannel.overwrites_for(newRole)
             overwrite = discord.PermissionOverwrite()
             overwrite.connect = False
-            #if there is a reason to make chats hidden until joined then uncomment this
-            #overwrite.read_messages = False
+            # if there is a reason to make chats hidden until joined then uncomment this
+            # overwrite.read_messages = False
             
             for role in roles:
                 if role.is_everyone:
                     await client.edit_channel_permissions(newChannel, role, overwrite)
                     break
 
+        if message.content.startswith("!removeLanguage"):
+            language = "language_" + message.content.replace("!registerLanguage", "").lower()
+            for server in client.servers:
+                client.delete_role(server, language)
+                client.delete_channel(language)
+            await client.send_message(message.channel, "Removed " + language)
+
     if isPlayer:
         # player specific commands
         # print("check that the language role already exists, and add the appropriate role to the user")
         if message.content.startswith("!iknow"):
-            language = message.content.replace("!iknow", "").strip()
-            thisList = ["DM", "Player", "admin", "bot"]
+            language = "language_" + message.content.replace("!iknow", "").lower()
             roleSet = 0
             for server in client.servers:
                 for role in server.roles:
-                    if language in role.name and language not in thisList:
-                        await client.send_message(message.channel, message.author.name + " now knows " + language)
+                    if language in role.name:
+                        await client.send_message(message.channel, message.author.name + " now knows " + language[9:])
                         await client.add_roles(message.author, role)
                         roleSet = 1
                         break
@@ -79,10 +87,10 @@ async def on_message(message):
     # everyone commands
     if message.content.startswith("!help"):
         DMCommands = "DM Commands\n"
-        DMCommands += "!registerLanguage language_name \n\t- Adds a language channel, " \
-                      "users must know this language in order to join this voice channel\n"
+        DMCommands += "!registerLanguage language_name \n\t- Adds a language channel, users must know this language in order to join this voice channel\n"
+        DMCommands += "!deleteLanguage language_name \n\t- Removes language channel and roles associated to that language\n"
         PlayerCommands = "Player Commands\n"
-        PlayerCommands += "!registerLanguage language_name \n\t- Adds your player to the language role\n"
+        PlayerCommands += "!iknow language_name \n\t- Adds your player to the language role\n"
         DMCommands + PlayerCommands
         await client.send_message(message.channel, DMCommands +PlayerCommands)
 
@@ -96,7 +104,7 @@ async def on_member_join(member):
 @client.event
 async def on_ready():
     for server in client.servers:
-        #first ensure that the roles exist for DM and players
+        # first ensure that the roles exist for DM and players
         hasDM = False
         hasPlayer = False
         DMRole = None
@@ -112,7 +120,7 @@ async def on_ready():
             DMRole = await client.create_role(server, name="DM")
         if hasPlayer == False:
             PlayerRole = await client.create_role(server, name="Player")
-        #now that the roles exist give the DM role to the creator of the sever, and player to all other members
+        # now that the roles exist give the DM role to the creator of the sever, and player to all other members
         for member in client.get_all_members():
             if member.server_permissions.administrator:
                 await client.add_roles(member, DMRole)
