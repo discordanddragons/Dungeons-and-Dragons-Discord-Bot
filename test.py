e9498180913d02585dea0ae5cbfa192a35fc1663
estebanmagallonperez <esteban1995perez@gmail.com>
2019-04-14 11:12:53 -0700

    Updating the Test file to use a unified Test function, and report the result of a given test

--- test.py
+++ test.py
@@ -1,57 +1,48 @@
 from characterManager import characterManager
 
 from characterManager import racemanager
+from characterManager import Utility
 characterName = "Gimli"
 authorName = "Esteban"
 
-def estebanTests():
+def test(testName,condition,expectedValue = True):
+    if condition == expectedValue:
+        print("\t"+testName +": PASS")
+        return True
+    else:
+        print(testName +": FAIL")
+        return False
+
+def characterManagerTest():
+    print("\ncharacterManager Test Start")
+    status = True
     manager = characterManager()
     #Test Character Adding
-    if manager.addCharacter(characterName,authorName):
-        print("addCharacter: PASS")
-    else:
-        print("addCharacter: FAIL")
-
-    if not manager.addCharacter(characterName,authorName):
-        print("duplicate addCharacter: PASS")
-    else:
-        print("duplicate addCharacter: FAIL")
+    status &= test("addCharacter",manager.addCharacter(characterName,authorName))
+    status &= test("duplicate addCharacter",manager.addCharacter(characterName,authorName),False)
 
     #Test setActive
-    if not manager.setActive("Not in dict","Not in dict"):
-        print("setActive: PASS")
-    else:
-        print("setActive: FAIL")
+    status &= test("setActive",manager.setActive("Not in dict","Not in dict"),False)
+    status &= test("getActive",manager.getActive(authorName),False)
+    status &= test("setActive",manager.setActive(characterName,authorName))
+    status &= test("getActive",manager.getActive(authorName),characterName)
 
-    if manager.getActive(authorName) == False:
-        print("getActive: Pass")
-    else:
-        print("getActive: FAIL")
+    test("characterManager Test",status)
 
-    if manager.setActive(characterName,authorName):
-        print("setActive: PASS")
-    else:
-        print("setActive: FAIL")
 
-    if manager.getActive(authorName) == characterName:
-        print("getActive: Pass")
-    else:
-        print("getActive: FAIL")
+def UtilityTest():
+    print("\nUtility Test Start")
+    status = True
+    utility = Utility()
 
-    #Utility Tests
-    if manager.utility.roll(1, 20) is None:
-        print("Roll: FAIL")
-    else:
-        print("Roll: Pass")
-
-    if manager.utility.flip() is None:
-        print("flip: FAIL")
-    else:
-        print("flip: Pass")
+    status &= test("Roll",utility.roll(1, 20) is None,False)
+    status &= test("Flip",utility.flip() is None,False)
+    test("Utility Test",status)
 
      #Test setRace
     # manager.setRace(authorName,"Minotaur")
     # manager.roll(authorName,"str")
 
 
-estebanTests()
+characterManagerTest()
+UtilityTest()
