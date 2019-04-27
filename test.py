d71ba8af0adf9b1411398e2a29e2bf0dee8a24de
estebanmagallonperez <esteban1995perez@gmail.com>
2019-04-27 16:49:19 -0700

    adding classManager to assist in selecting a class

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
-from characterManager import characterManager
 
-from characterManager import racemanager
-from characterManager import Utility
+from characterManager import characterManager
+from characterManager import roll
+from characterManager import flip
+
+from characterManager import raceManager
+from characterManager import classManager
 characterName = "Gimli"
 authorName = "Esteban"
 
@@ -29,14 +32,28 @@ def characterManagerTest():
 
     test("characterManager Test",status)
 
+def raceManagerTest():
+    raceMgr = raceManager()
+    print("\nracemanager Test Start")
+    status  = True
+    status &= test("load races",len(raceMgr.races) > 0)
+    test("raceManager Test",status)
+
+def classManagerTest():
+    classMgr = classManager()
+    print("\nclassManager Test Start")
+    status  = True
+    status &= test("load classes",len(classMgr.classes) > 0)
+    print(classMgr)
+    test("classManager Test",status)
+
 
 def UtilityTest():
     print("\nUtility Test Start")
     status = True
-    utility = Utility()
 
-    status &= test("Roll",utility.roll(1, 20) is None,False)
-    status &= test("Flip",utility.flip() is None,False)
+    status &= test("Roll",roll(1, 20) is None,False)
+    status &= test("Flip",flip() is None,False)
     test("Utility Test",status)
 
      #Test setRace
@@ -46,3 +63,5 @@ def UtilityTest():
 
 characterManagerTest()
 UtilityTest()
+raceManagerTest()
+classManagerTest()
